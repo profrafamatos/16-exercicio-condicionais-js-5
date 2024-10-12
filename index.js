4c9fc2a91beafe3ab955652534c3d69eba7899ee
profrafamatos <rcmm@cesar.school>
2024-10-12 14:12:10 -0300

    Arquivos necessários para resolver o exercício

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ Implemente uma função chamada 'classificacaoMedia' que recebe um número 
+ representando a media semestral de um aluno (media) e retorna:
+    "Aprovado" se a media for maior ou igual a 7,
+    "Aguardando a final" se a media for maior ou igual a 4 e menor que 7,
+    "Reprovado" se a media for menor que 4. 
+ 
+    Sintaxe:
+      switch(valor):
+        case valor:
+          return "alguma coisa"
+        default:
+          return "alguma outra coisa"
+ */
+
+function classificacaoNota(media) {
+    /**** Escreva o código abaixo 👇******/
+    
+
+    /**** Escreva o código acima ☝️******/
+  }
+  
+  module.exports = classificacaoNota; //Não altere essa linha
+  
